d749914512cdb365424833c890a8d3e83a091936
tom-wheeldon <tomwheeldon@gmail.com>
2024-07-10 13:18:41 +0100

    still testing mesh

--- main.js
+++ main.js
@@ -7,31 +7,20 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById('animation-container').appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry();
+    const geometry = new THREE.SphereGeometry(5, 32, 32);
     const material = new THREE.MeshBasicMaterial({ color: 0x0077ff, wireframe: true });
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
-    camera.position.z = 5;
-
-    window.addEventListener('resize', onWindowResize, false);
-
-    animate();
-}
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.z = 20;
 }
 
 function animate() {
     requestAnimationFrame(animate);
-
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.01;
-
     renderer.render(scene, camera);
 }
 
 init();
+animate();
